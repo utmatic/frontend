baef3e5957bd9b62eaa2e1b27cb4c871ea9c043f
utmatic <john.snively@outlook.com>
2025-05-23 11:36:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const TAG_ICON = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0
 const LINK_ICON = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="height:1.15em;vertical-align:-0.13em;"><path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"/></svg>`;
 
 let baseUrlMemory = JSON.parse(localStorage.getItem('baseUrlMemory') || "[]");
-let baseUrlMemory = JSON.parse(localStorage.getItem('baseUrlMemory') || "[]");
 function updateBaseUrlMemory(newUrl) {
   if (newUrl && !baseUrlMemory.includes(newUrl)) {
     baseUrlMemory.push(newUrl);
@@ -565,7 +564,7 @@ Each match is combined with the Base URL:<br>
   mappingToolLink.style.cursor = "pointer";
   mappingToolLink.style.color = "#2563eb";
   mappingToolLink.style.fontWeight = "600";
-  mappingToolLink.style.marginBottom = "0.5em";
+  mappingToolLink.style.marginBottom = "1.2em";
   mappingToolLink.style.alignSelf = "flex-start";
   mappingToolLink.innerHTML = `Open Mapping Area Tool <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:1.2em;height:1.2em;vertical-align:-0.21em;"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>`;
   // --- Underline Checkbox ---
