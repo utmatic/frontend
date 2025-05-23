55fa3338495848891e7c2fd6ee7efc98c687667c
utmatic <john.snively@outlook.com>
2025-05-23 12:20:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -565,9 +565,9 @@ Each match is combined with the Base URL:<br>
   mappingToolLink.style.cursor = "pointer";
   mappingToolLink.style.color = "#2563eb";
   mappingToolLink.style.fontWeight = "500";
-  mappingToolLink.style.marginBottom = "1.2em";
+  mappingToolLink.style.marginBottom = "1.3em";
   mappingToolLink.style.alignSelf = "flex-start";
-  mappingToolLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:1.2em;height:1.2em;vertical-align:-0.21em;margin-right:0.4em;"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>Open Mapping Area Tool`;
+  mappingToolLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:1.2em;height:1.2em;vertical-align:-0.21em;margin-right:0.4em;"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>Mapping tool`;
 
   // --- Underline Checkbox ---
   const underlineWrapper = document.createElement("div");
