4a4807174062606988856f7aad1ec388c4a8a71a
utmatic <john.snively@outlook.com>
2025-05-23 11:45:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -567,7 +567,7 @@ Each match is combined with the Base URL:<br>
   mappingToolLink.style.fontWeight = "600";
   mappingToolLink.style.marginBottom = "1.2em";
   mappingToolLink.style.alignSelf = "flex-start";
-  mappingToolLink.innerHTML = `Open Mapping Area Tool <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:1.2em;height:1.2em;vertical-align:-0.2em;"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/></svg>`;
+  mappingToolLink.innerHTML = `Open Mapping Area Tool <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:1.2em;height:1.2em;vertical-align:-0.21em;"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>`;
 
   // --- Underline Checkbox ---
   const underlineWrapper = document.createElement("div");
