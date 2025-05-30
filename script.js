ff49f54f6aaffa4aff177a0da3f9f8b12bd548f1
utmatic <john.snively@outlook.com>
2025-05-30 15:45:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ window.pdfjsViewer =
   window['pdfjs-dist/web/pdf_viewer'] ||
   undefined;
 
-// SVG ICONS
-const TAG_ICON = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="height:1.15em;vertical-align:-0.13em;" stroke-width="1.7" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"/></svg>`;
-const LINK_ICON = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="height:1.15em;vertical-align:-0.13em;"><path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"/></svg>`;
-
 let baseUrlMemory = JSON.parse(localStorage.getItem('baseUrlMemory') || "[]");
 function updateBaseUrlMemory(newUrl) {
   if (newUrl && !baseUrlMemory.includes(newUrl)) {
@@ -126,19 +122,6 @@ function updateFileListStatus(tabId, name, saved, jobType) {
     item = document.createElement("li");
     item.dataset.tab = tabId;
     fileList.appendChild(item);
-  }
-  let jobIcons = item.querySelector('.job-icons');
-  if (!jobIcons) {
-    jobIcons = document.createElement('span');
-    jobIcons.className = 'job-icons';
-    item.insertBefore(jobIcons, item.firstChild);
-  }
-  // Update job icons
-  jobIcons.innerHTML = "";
-  if (jobType === "utm_only") {
-    jobIcons.innerHTML = TAG_ICON;
-  } else if (jobType === "links_and_utm") {
-    jobIcons.innerHTML = LINK_ICON;
   }
   let existingCheck = item.querySelector(".checkmark");
   if (existingCheck) existingCheck.remove();
