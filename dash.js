b908018acdd631817627eb764d8edad88ddc41c7
utmatic <john.snively@outlook.com>
2025-06-04 21:05:53 -0400

    Update dash.js

--- dash.js
+++ dash.js
@@ -19,7 +19,7 @@ let jobs = [];
 let jobsLoaded = false; // Track if jobs are already loaded
 
 // --- Time Save & Counter Logic ---
-const SECONDS_PER_LINK = 30;
+const SECONDS_PER_LINK = 45;
 const UNITS = [
   { name: "minutes", label: "Minutes", factor: 60 },
   { name: "hours", label: "Hours", factor: 3600 },
@@ -29,7 +29,6 @@ let currentUnit = UNITS[0]; // default to minutes
 
 function computeTimeSaved(jobs, unitObj = currentUnit) {
   if (!Array.isArray(jobs)) return 0;
-  // Use 'linkCount' (camelCase) as per your backend API
   const totalLinks = jobs.reduce((sum, job) => sum + (parseInt(job.linkCount) || 0), 0);
   const secondsSaved = totalLinks * SECONDS_PER_LINK;
   return secondsSaved / unitObj.factor;
@@ -179,10 +178,14 @@ function renderHistorySnapshot(jobs) {
       <td>${beautifyJobType(job.jobtype)}</td>
       <td>
         <a href="${job.processedUrl ?? '#'}" class="download-link" download title="Download file">
-          <!-- download icon -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke="currentColor" class="download-icon" width="22" height="22">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
+          </svg>
         </a>
         <a href="${job.changelogUrl ?? '#'}" class="download-link" download title="Download report">
-          <!-- report icon -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke="currentColor" class="download-icon" width="22" height="22">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+          </svg>
         </a>
       </td>
     </tr>
@@ -190,13 +193,6 @@ function renderHistorySnapshot(jobs) {
   return `
     <div class="history-list">
       <table class="history-table">
-        <colgroup>
-          <col style="width: 180px;">   <!-- Date column wider -->
-          <col style="width: 60px;">    <!-- File Type column shorter -->
-          <col>                         <!-- Document Name (flex) -->
-          <col style="width: 140px;">   <!-- Job Type -->
-          <col style="width: 120px;">   <!-- Downloads -->
-        </colgroup>
         <thead>
           <tr>
             <th>Date</th>
@@ -226,10 +222,14 @@ function renderHistory(jobs) {
       <td>${beautifyJobType(job.jobtype)}</td>
       <td>
         <a href="${job.processedUrl ?? '#'}" class="download-link" download title="Download file">
-          <!-- download icon -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke="currentColor" class="download-icon" width="22" height="22">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
+          </svg>
         </a>
         <a href="${job.changelogUrl ?? '#'}" class="download-link" download title="Download report">
-          <!-- report icon -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke="currentColor" class="download-icon" width="22" height="22">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+          </svg>
         </a>
       </td>
     </tr>
@@ -237,13 +237,6 @@ function renderHistory(jobs) {
   return `
     <div class="history-list">
       <table class="history-table">
-        <colgroup>
-          <col style="width: 180px;">   <!-- Date column wider -->
-          <col style="width: 60px;">    <!-- File Type column shorter -->
-          <col>                         <!-- Document Name (flex) -->
-          <col style="width: 140px;">   <!-- Job Type -->
-          <col style="width: 120px;">   <!-- Downloads -->
-        </colgroup>
         <thead>
           <tr>
             <th>Date</th>
