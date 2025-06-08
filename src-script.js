d31d777b4c5bfa0aac303cf70f830f5df43220ad
utmatic <john.snively@outlook.com>
2025-06-08 14:31:04 -0400

    Update src-script.js

--- src-script.js
+++ src-script.js
@@ -153,9 +153,9 @@ function showInactivityModal() {
   const modalBox = document.createElement('div');
   modalBox.className = "inactivity-modal-box";
 
-  // "Your session is about to expire"
+  // "Automatic logout"
   const heading = document.createElement('h3');
-  heading.textContent = "Your session is about to expire";
+  heading.textContent = "Automatic logout in";
   modalBox.appendChild(heading);
 
   // (TIMER VALUE) - emphasized
